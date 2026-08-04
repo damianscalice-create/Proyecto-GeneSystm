ae43c64fc2b650e002915ab78ffe89d82cef531b
Damian Tesauro <damianscalice@gmail.com>
2026-08-04 00:53:48 -0300

    cambio de colores en el sidebar

--- js/torneos.js
+++ js/torneos.js
@@ -98,3 +98,19 @@ if (deporteSelect && cantidadSelect) {
 if (botonConfirmar) {
     botonConfirmar.addEventListener('click', mostrarMensajeConfirmacion);
 }
+
+/* Script para abrir el sidebar */
+
+const botonAbrir = document.getElementById('botonAbrir');
+const botonCerrar = document.getElementById('botonCerrar');
+const sidebar = document.getElementById('miSidebar');
+
+if (botonAbrir && botonCerrar && sidebar) {
+    botonAbrir.addEventListener('click', () => {
+        sidebar.classList.add('activo');
+    });
+
+    botonCerrar.addEventListener('click', () => {
+        sidebar.classList.remove('activo');
+    });
+}
